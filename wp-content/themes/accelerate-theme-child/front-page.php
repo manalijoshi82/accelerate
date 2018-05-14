a75b9b3488395c47f470b0b4ce2931dfcb779d7c
manali <manalijoshi82@gmail.com>
2018-05-14 13:28:27 -0700

    modified accelerate

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,4 +25,19 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+	<section class="recent-posts">
+		<div class="site-content">
+			<div class="blog-post">
+				<h4>From the blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+					<?php the_excerpt(); ?>
+				<?php endwhile; ?> 
+			 <?php wp_reset_query(); ?>
+			</div>
+		</div>
+	</section>
+
+
 <?php get_footer(); ?>
